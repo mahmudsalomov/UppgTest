22623ec0d420c3fc1c0eaab6c16e281c600345f3
mahmudsalomov <mahmud.salomov.salomov@gmail.com>
2022-06-02 10:36:00 +0500

    Initial commit

---
+++ src/main/java/uz/maniac4j/uppgtest/UppgTestApplication.java
@@ -0,0 +1,13 @@
+package uz.maniac4j.uppgtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UppgTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UppgTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/maniac4j/uppgtest/UppgTestApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.maniac4j.uppgtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UppgTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
